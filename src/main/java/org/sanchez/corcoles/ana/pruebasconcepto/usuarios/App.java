415838d7c8aa542b0b0a5f925447b80a56199217
Ana Córcoles <ana.corcoles@gmail.com>
2019-11-03 14:08:43 +0100

    Add files via upload

--- src/main/java/org/sanchez/corcoles/ana/pruebasconcepto/usuarios/App.java
+++ src/main/java/org/sanchez/corcoles/ana/pruebasconcepto/usuarios/App.java
@@ -2,10 +2,12 @@ package org.sanchez.corcoles.ana.pruebasconcepto.usuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EntityScan({"org.sanchez.corcoles.ana.pruebasconcepto.usuarios.commons.model.entity"})
 public class App {
 
     public static void main(String[] args) {
