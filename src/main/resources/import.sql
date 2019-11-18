0d8d33d35e9013adb6a33d7ef7e0d6bbabb0bb0e
Ana Córcoles <ana.corcoles@gmail.com>
2019-11-18 19:54:56 +0100

    Add Sleuth dependency.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,7 @@
-INSERT INTO usuarios (user_name, nombre, apellido, enabled, email) VALUES ('corcoles155', 'Ana', 'Córcoles', true, 'ana.corcoles@gmail.com')
-INSERT INTO usuarios (user_name, nombre, apellido, enabled, email) VALUES ('andres', 'Andres', 'Sánchez', true, 'andres.sanchez@gmail.com')
+INSERT INTO usuarios (user_name, nombre, apellido, enabled, email, password) VALUES ('corcoles155', 'Ana', 'Córcoles', true, 'ana.corcoles@gmail.com', '$2a$10$N2ExTJxur1So6IMg41dcguN3iGP3mrMQCorH8nYudo8MPfF5kJs4G')
+INSERT INTO usuarios (user_name, nombre, apellido, enabled, email, password) VALUES ('andres133', 'Andres', 'Sánchez', true, 'andres.sanchez@gmail.com', '$2a$10$g4o6Upx4.DuwaYB/HckvSON/HYFsEk0VNLu5PNl2DR.llg2gr7Lvu')
+INSERT INTO usuarios (user_name, nombre, apellido, enabled, email, password) VALUES ('pepe122', 'Pepe', 'Díaz', true, 'pepe.diaz@gmail.com', '$2a$10$XBbXIQ9j9itr8NDwFRFa8.LWL3m54zg0nnf4f94Znb2FhjGsQaRMq')
+INSERT INTO usuarios (user_name, nombre, apellido, enabled, email, password) VALUES ('juan455', 'Juan', 'Ruiz', true, 'juan.ruiz@gmail.com', '$2a$10$fsPTBK3FryQqoXdpgS8Q8.M5gU/SYiKwXTszqTM5lAboYEE.kmeNy')
 
 INSERT INTO roles (nombre) VALUES ('ROLE_USER')
 INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN')
